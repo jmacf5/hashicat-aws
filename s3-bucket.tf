44dff514e678b469e01418843bfea9e2f460240d
JeffM <j.macfarland@f5.com>
2021-01-13 18:25:29 +0000

    Set prefix to var

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/jmac-f5networks/s3-bucket/aws"
   version = "1.17.0"
   # insert required variables here
-  bucket_prefix = "jmac-f55-r"
+  bucket_prefix = "${vars.prefix}"
 }
