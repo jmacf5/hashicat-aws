69c5428fd6c865217dc4f801b839871f55e15a7f
jmacf5 <77363042+jmacf5@users.noreply.github.com>
2021-01-13 08:28:03 -0800

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. It's not what you think it is.
+  Welcome to ${PREFIX}'s app. Someone said there would be ice cream.
   <!-- END -->
 
   </div>
